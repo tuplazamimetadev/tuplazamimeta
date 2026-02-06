6271255ab1389ed53782b04cd6916fddf41cb5e7
tuplazamimetadev <tuplazamimetadev@gmail.com>
2026-02-06 19:12:36 +0100

    arreglo columnas question

--- src/main/java/com/tuplazamimeta/policiaapi/model/Question.java
+++ src/main/java/com/tuplazamimeta/policiaapi/model/Question.java
@@ -14,13 +14,21 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String statement;
 
+    // --- MAPEO EXPLÍCITO DE COLUMNAS ---
+    @Column(name = "option_a") // Aseguramos que busque "option_a"
     private String optionA;
+
+    @Column(name = "option_b")
     private String optionB;
+
+    @Column(name = "option_c")
     private String optionC;
+
+    @Column(name = "option_d")
     private String optionD;
-    
-    @Column(name = "correct_option", columnDefinition = "CHAR(1)")
-    private String correctOption; 
+
+    @Column(name = "correct_option", columnDefinition = "CHAR(1)") // CHAR(1) como vimos antes
+    private String correctOption;
 
     @Column(columnDefinition = "TEXT")
     private String explanation;
